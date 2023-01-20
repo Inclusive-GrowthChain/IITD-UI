2fbfa8a8637de73f91bb9abeb78be742492121f6
Raj kumar <rajkumar@inclusivegrowthchain.com>
2023-01-20 07:00:14 +0530

    farmer additional 4 pages restrcture

---
+++ src/comp/Farmer/SellingPrice.js
@@ -0,0 +1,155 @@
+function SellingPrice() {
+  return (
+    <div className="itemContainer">
+      <div className="list_title">
+        <div className="container-fluid">
+          <div className="d-sm-flex justify-content-between align-items-center mb-4">
+            <h3
+              className="text-dark mb-0"
+              style={{
+                fontSize: "22px",
+                fontWeight: "800",
+                color: "rgb(33, 37, 41)",
+              }}
+            >
+              Sell to FPO
+            </h3>
+          </div>
+          <div className="row row-cols-1 row-cols-lg-3 row-cols-md-2 g-4">
+            <div className="col">
+              <div className="card">
+                <img
+                  src="https://3.imimg.com/data3/TM/JM/ETO-196508/1-294285-full-images-stick-lac-1081592-500x500.jpg"
+                  className="card-img-top"
+                  alt=""
+                  style={{ height: "280px", objectFit: "cover" }}
+                />
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col">
+                      <h4>Stick Lac</h4>
+                    </div>
+                  </div>
+                  <div className="row mt-2">
+                    <div className="col-7">
+                      <div className="row">
+                        <div className="col">
+                          <h5>Market Price</h5>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <span>500</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-5">
+                      <div className="row">
+                        <div className="col">
+                          <h5>FPO Price</h5>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <span>500</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img
+                  src="https://img3.exportersindia.com/product_images/bc-full/2018/9/1491297/seedlac-shellac-1536140025-4266463.jpg"
+                  className="card-img-top"
+                  alt=""
+                  style={{ height: "280px", objectFit: "cover" }}
+                />
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col">
+                      <h4>Stick Lac</h4>
+                    </div>
+                  </div>
+                  <div className="row mt-2">
+                    <div className="col-7">
+                      <div className="row">
+                        <div className="col">
+                          <h5>Market Price</h5>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <span>500</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-5">
+                      <div className="row">
+                        <div className="col">
+                          <h5>FPO Price</h5>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <span>500</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              <div className="card">
+                <img
+                  src="https://5.imimg.com/data5/WH/TW/MY-1496311/shellac-processed-versatile-lac-golden-kusmi-500x500.jpg"
+                  className="card-img-top"
+                  alt=""
+                  style={{ height: "280px", objectFit: "cover" }}
+                />
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col">
+                      <h4>Stick Lac</h4>
+                    </div>
+                  </div>
+                  <div className="row mt-2">
+                    <div className="col-7">
+                      <div className="row">
+                        <div className="col">
+                          <h5>Market Price</h5>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <span>500</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-5">
+                      <div className="row">
+                        <div className="col">
+                          <h5>FPO Price</h5>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col">
+                          <span>500</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SellingPrice
---
+++ src/comp/Template/Support.js
@@ -0,0 +1,22 @@
+function Support() {
+  return (
+    <div className="itemContainer">
+      <div className="list_title">
+        <div className="setting_data flex-1 container-fluid">
+          <textarea
+            className="setting_content"
+            placeholder="Enter your queries..."
+          ></textarea>
+
+          <button className="seting_btn bg-[#a3dc5d] hover:bg-[#74a03e]">
+            Submit
+          </button>
+        </div>
+
+        <div className="df px-8 h-[inherit]"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Support
