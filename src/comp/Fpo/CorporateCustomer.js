fdfbbafc59d2d01364570219e6c79e24d5bce575
PBYinclusivegrowthchain <praveen.yaragatti@inclusivegrowthchain.com>
2024-12-02 13:45:32 +0530

    fixing the total amount in fpo completed transactions

--- src/comp/Fpo/CorporateCustomer.js
+++ src/comp/Fpo/CorporateCustomer.js
@@ -228,7 +228,7 @@ const CorporateCustomer = () => {
                                 <td>{bid.bidId}</td>
                                 <td>{b.clientInvoiceDate?.substring(0, 10)}</td>
                                 <td>{b.clientInvoiceNumber}</td>
-                                <td>{ parseInt(b.bidAmount) * b.quantity}</td>
+                                <td>{ parseInt(b.bidAmount) * bid.quantity}</td>
                                 <td>
                                   <button
                                     style={btnStyle}
