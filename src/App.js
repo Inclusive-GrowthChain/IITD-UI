341ad10e4050b3440ddab2190904399f8300f5b3
PBYinclusivegrowthchain <praveen.yaragatti@inclusivegrowthchain.com>
2024-12-02 20:23:03 +0530

    fixing the route for admin role

--- src/App.js
+++ src/App.js
@@ -185,7 +185,7 @@ function App() {
 
         {/* Admin route */}
         <Route element={<ProtectedRoute role={"admin"}/>}>
-        <Route path="admin" element={<AdminWrapper />}>
+        <Route path="/admin" element={<AdminWrapper />}>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="license-key" element={<LicenseKey />} />
         </Route>
