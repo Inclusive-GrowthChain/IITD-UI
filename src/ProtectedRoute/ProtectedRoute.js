46d5a5bfbec0f022d3cb7ebf8587e3069583929e
PBYinclusivegrowthchain <praveen.yaragatti@inclusivegrowthchain.com>
2024-12-02 20:56:23 +0530

    fixing the route for admin role

--- src/ProtectedRoute/ProtectedRoute.js
+++ src/ProtectedRoute/ProtectedRoute.js
@@ -6,7 +6,7 @@ const ProtectedRoute = ({ role }) => {
   const loggedIn = useAuthStore((auth) => auth.loggedIn);
   const userRole = useAuthStore((user) => user.userDetails);
 
-  console.log(userRole.userType, role,"Role of the route")
+  console.log(userRole, role,"Role of the route")
   return loggedIn && userRole.userType === role ? <Outlet /> : <Navigate to="/" />;
 };
 
